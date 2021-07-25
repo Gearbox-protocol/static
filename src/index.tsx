3d46ab2b8c907f7a3bb55254a5493ef2e6af126c
Mikhail Lazarev <mikael.lazarev@gmail.com>
2021-07-26 00:32:31 +0300

    v0.0.6

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+export { IconCard } from "./src/icons/card";
+export { IconClose } from "./src/icons/close";
+export { IconExpand } from "./src/icons/expand";
+export { IconGear } from "./src/icons/gear";
+export { IconGraph } from "./src/icons/graph";
+export { IconHistory } from "./src/icons/history";
+export { IconTrianle } from "./src/icons/triangle";
+
+export {IconBook} from "./src/menu/book"
+export {IconCheck} from "./src/menu/check"
+export {IconCreditCard} from "./src/menu/credit-card"
+export {IconEarn} from "./src/menu/earn"
+export {IconMessage} from "./src/menu/message"
+export {IconPieChart} from "./src/menu/piechart"
+export {IconSquare} from "./src/menu/square"
+export {IconZap} from "./src/menu/zap"
