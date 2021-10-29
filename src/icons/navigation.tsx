9b0ddb7fa25437955fa62895984fdb9394772196
essserrr <blizzz93@gmail.com>
2021-10-29 20:42:29 +0900

    Navigation icon added

---
+++ src/icons/navigation.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { IconProps } from '../core/icon';
+
+export const IconNavigation = ({ size, style, onClick }: IconProps) => (
+  <svg
+    style={style}
+    onClick={onClick}
+    xmlns="http://www.w3.org/2000/svg"
+    width={size || 24}
+    height={size || 24}
+    viewBox="0 0 24 24"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke="#ffffff"
+    >
+      <polygon points="3 11 22 2 13 21 11 13 3 11"></polygon>
+  </svg>
+);
