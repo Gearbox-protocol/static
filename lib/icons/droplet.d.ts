58c71d32cca779b48cccf3fc24fdd13d6ed61980
Mikhail Lazarev <mikael.lazarev@gmail.com>
2021-12-21 09:23:39 +0100

    update

---
+++ lib/icons/droplet.d.ts
@@ -0,0 +1,3 @@
+import React from "react";
+import { IconProps } from "../core/icon";
+export declare function IconDroplet({ size, style, onClick, }: IconProps): React.ReactElement;
---
+++ lib/icons/edit.d.ts
@@ -0,0 +1,3 @@
+import React from "react";
+import { IconProps } from "../core/icon";
+export declare function IconEdit({ size, style, onClick, }: IconProps): React.ReactElement;
