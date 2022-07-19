554a49d7b5a5adb99ea11af5885f9d80bc63e9d2
esser <blizzz93@gmail.com>
2022-07-19 15:24:12 +0900

    new component icons

---
+++ lib/icons/chart.d.ts
@@ -0,0 +1,2 @@
+import { IconProps } from "../core/icon";
+export declare const IconChart: ({ style, onClick, size }: IconProps) => JSX.Element;
---
+++ lib/icons/fire.d.ts
@@ -0,0 +1,2 @@
+import { IconProps } from "../core/icon";
+export declare const IconFire: ({ style, onClick, size }: IconProps) => JSX.Element;
