8b4026e998de81373e0b7fcf87a1e1326dd94895
Mikael <mikael.lazarev@gmail.com>
2022-07-30 12:16:33 +0200

    rr

--- lib/icons/close.d.ts
+++ lib/icons/close.d.ts
@@ -1,2 +1,3 @@
+/// <reference types="react" />
 import { IconProps } from "../core/icon";
 export declare const IconClose: ({ style, onClick, size }: IconProps) => JSX.Element;
--- lib/menu/book.d.ts
+++ lib/menu/book.d.ts
@@ -1,2 +1,3 @@
+/// <reference types="react" />
 import { IconProps } from "../core/icon";
 export declare const IconBook: ({ style, onClick, size }: IconProps) => JSX.Element;
