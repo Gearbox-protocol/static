50a8d322c6066cef1a24b37d4d87fcbc7b041b3a
esser <blizzz93@gmail.com>
2022-11-03 17:15:11 +0900

    fix: test pr

--- src/icons/pending.tsx
+++ src/icons/pending.tsx
@@ -3,20 +3,18 @@ import { Settings } from "react-feather";
 
 import { IconProps } from "../core/icon";
 
-export const IconPending = ({ size, style, onClick }: IconProps) => {
-  return (
-    <Settings
-      size={size}
-      onClick={onClick}
-      style={{
-        marginTop: "2px",
-        animationDuration: "3s",
-        animationName: "spin",
-        animationIterationCount: "infinite",
-        animationDelay: "0",
-        animationTimingFunction: "linear",
-        ...style,
-      }}
-    />
-  );
-};
+export const IconPending = ({ size, style, onClick }: IconProps) => (
+  <Settings
+    size={size}
+    onClick={onClick}
+    style={{
+      marginTop: "2px",
+      animationDuration: "3s",
+      animationName: "spin",
+      animationIterationCount: "infinite",
+      animationDelay: "0",
+      animationTimingFunction: "linear",
+      ...style,
+    }}
+  />
+);
