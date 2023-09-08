0f7e3be9e21a21a62cb6c03079f01d3bbb79ff5d
esser <blizzz93@gmail.com>
2023-09-08 11:30:43 +0800

    feat: add swap and farm icons

---
+++ src/icons/swap.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { IconProps } from "../core/icon";
+
+export const IconSwap = ({ style, onClick, size = 24 }: IconProps) => (
+  <svg
+    width={size}
+    height={size}
+    style={style}
+    onClick={onClick}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M8 10V13H14V18H8V21L2 15.5L8 10ZM22 8.5L16 3V6H10V11H16V14L22 8.5Z"
+      fill="currentColor"
+    />
+  </svg>
+);
