887dad4dbdbb8194a91ec11b858d8e4de6656d50
esser <blizzz93@gmail.com>
2025-09-03 18:02:40 +0700

    fix: success icon props

--- src/icons/successCircle.tsx
+++ src/icons/successCircle.tsx
@@ -2,12 +2,22 @@ import React from "react";
 
 import type { IconProps } from "../core/icon";
 
+interface SuccessCircleProps extends IconProps {
+  fill?: string;
+  strokeMark?: string;
+  strokeCircle?: string;
+}
+
 export const SuccessCircle = ({
   style,
   onClick,
   size = 24,
   className,
-}: IconProps) => (
+
+  fill = "#92FFB0",
+  strokeMark = "#171D2D",
+  strokeCircle,
+}: SuccessCircleProps) => (
   <svg
     className={className}
     width={size}
@@ -18,10 +28,10 @@ export const SuccessCircle = ({
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <circle cx="12" cy="12" r="12" fill="#92FFB0" />
+    <circle cx="12" cy="12" r="12" fill={fill} stroke={strokeCircle} />
     <path
       d="M7 13L9.64645 15.6464C9.84171 15.8417 10.1583 15.8417 10.3536 15.6464L17 9"
-      stroke="#171D2D"
+      stroke={strokeMark}
       strokeWidth="2"
       strokeLinecap="round"
     />
