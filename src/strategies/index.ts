2d029cc486c5753cba76bdfdea1772313f15dc2f
de-snake <122296842+de-snake@users.noreply.github.com>
2025-09-10 16:03:10 +0300

    Update index.ts

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -66,7 +66,7 @@ export const STRATEGIES_LIST: Array<StrategyConfigPayload> = [
       "0xbcd2ffac58189e57334bb63253acbf34d776de53",
       "0x8c118e8c20cebbaa2467b735bbb8b13d614e6608",
       "0x92660c7B0925b8b17C6E5696e58FE9F8cC038E21",
-      "0x38811f98eDe771137DC71f046E0e300b7014C5C7"
+      "0x38811f98eDe771137DC71f046E0e300b7014C5C7",
     ],
     issuesOnClose: true,
   },
@@ -81,7 +81,7 @@ export const STRATEGIES_LIST: Array<StrategyConfigPayload> = [
       "0xbcd2ffac58189e57334bb63253acbf34d776de53",
       "0x8c118e8c20cebbaa2467b735bbb8b13d614e6608",
       "0x92660c7B0925b8b17C6E5696e58FE9F8cC038E21",
-      "0x38811f98eDe771137DC71f046E0e300b7014C5C7"
+      "0x38811f98eDe771137DC71f046E0e300b7014C5C7",
     ],
     releaseAt: 1755770400000,
     issuesOnClose: true,
@@ -97,7 +97,7 @@ export const STRATEGIES_LIST: Array<StrategyConfigPayload> = [
       "0xbcd2ffac58189e57334bb63253acbf34d776de53",
       "0x8c118e8c20cebbaa2467b735bbb8b13d614e6608",
       "0x92660c7B0925b8b17C6E5696e58FE9F8cC038E21",
-      "0x38811f98eDe771137DC71f046E0e300b7014C5C7"
+      "0x38811f98eDe771137DC71f046E0e300b7014C5C7",
     ],
     releaseAt: 1755770400000,
     issuesOnClose: true,
@@ -113,7 +113,7 @@ export const STRATEGIES_LIST: Array<StrategyConfigPayload> = [
       "0xbcd2ffac58189e57334bb63253acbf34d776de53",
       "0x8c118e8c20cebbaa2467b735bbb8b13d614e6608",
       "0x92660c7B0925b8b17C6E5696e58FE9F8cC038E21",
-      "0x38811f98eDe771137DC71f046E0e300b7014C5C7"
+      "0x38811f98eDe771137DC71f046E0e300b7014C5C7",
     ],
     releaseAt: 1755770400000,
     issuesOnClose: true,
@@ -139,7 +139,7 @@ export const STRATEGIES_LIST: Array<StrategyConfigPayload> = [
       "0xbcd2ffac58189e57334bb63253acbf34d776de53",
       "0x8c118e8c20cebbaa2467b735bbb8b13d614e6608",
       "0x92660c7B0925b8b17C6E5696e58FE9F8cC038E21",
-      "0x38811f98eDe771137DC71f046E0e300b7014C5C7"
+      "0x38811f98eDe771137DC71f046E0e300b7014C5C7",
     ],
     issuesOnClose: true,
   },
@@ -154,7 +154,7 @@ export const STRATEGIES_LIST: Array<StrategyConfigPayload> = [
       "0xbcd2ffac58189e57334bb63253acbf34d776de53",
       "0x8c118e8c20cebbaa2467b735bbb8b13d614e6608",
       "0x92660c7B0925b8b17C6E5696e58FE9F8cC038E21",
-      "0x38811f98eDe771137DC71f046E0e300b7014C5C7"
+      "0x38811f98eDe771137DC71f046E0e300b7014C5C7",
     ],
     issuesOnClose: true,
   },
@@ -169,7 +169,7 @@ export const STRATEGIES_LIST: Array<StrategyConfigPayload> = [
       "0xbcd2ffac58189e57334bb63253acbf34d776de53",
       "0x8c118e8c20cebbaa2467b735bbb8b13d614e6608",
       "0x92660c7B0925b8b17C6E5696e58FE9F8cC038E21",
-      "0x38811f98eDe771137DC71f046E0e300b7014C5C7"
+      "0x38811f98eDe771137DC71f046E0e300b7014C5C7",
     ],
     issuesOnClose: true,
   },
