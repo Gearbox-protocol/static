7570401b736ccef8db7dd9f5b16a280df4b820bc
de-snake <122296842+de-snake@users.noreply.github.com>
2025-10-07 16:50:00 +0300

    Update index.ts

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -448,8 +448,8 @@ export const STRATEGIES_LIST: Array<StrategyConfigPayload> = [
     creditManagers: [
       "0x50ba483272484fc5eebe8676dc87d814a11faef6",
       "0x06c0df5ac1f24bc2097b59ed8ee1db86bf0b09df",
-      "0x9a0fdf7cdab4604fc27ebeab4b3d57bd825e8ebe"
-      ],
+      "0x9a0fdf7cdab4604fc27ebeab4b3d57bd825e8ebe",
+    ],
     issuesOnClose: true,
   },
   {
@@ -593,7 +593,7 @@ export const STRATEGIES_LIST: Array<StrategyConfigPayload> = [
     creditManagers: [
       "0x50ba483272484fc5eebe8676dc87d814a11faef6",
       "0x06c0df5ac1f24bc2097b59ed8ee1db86bf0b09df",
-      "0x9a0fdf7cdab4604fc27ebeab4b3d57bd825e8ebe"
+      "0x9a0fdf7cdab4604fc27ebeab4b3d57bd825e8ebe",
     ],
     issuesOnClose: true,
   },
